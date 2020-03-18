8353acb98d8bc8c98abf2cf3222e779dd3e58f04
a17600765 <Vetchinnikov.P.E@sberbank.ru>
2020-03-18 22:57:25 +0300

    Поправлены импорты.

--- src/test/java/ru/otus/homework/service/QuestionsServiceImplTest.java
+++ src/test/java/ru/otus/homework/service/QuestionsServiceImplTest.java
@@ -1,12 +1,10 @@
 package ru.otus.homework.service;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import ru.otus.homework.dao.QuestionsDao;
 import ru.otus.homework.model.Question;
