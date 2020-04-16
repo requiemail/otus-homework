4389ea97ae44bbd2e011d56b375da269f1dc7964
a17600765 <Vetchinnikov.P.E@sberbank.ru>
2020-04-16 17:43:27 +0300

    Подготовлена основа второго проекта

---
+++ bookshelf/src/main/java/ru/otus/homework/BookShelf.java
@@ -0,0 +1,13 @@
+package ru.otus.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShelf {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookShelf.class, args);
+    }
+
+}
