966e69628796d0d0077e3f5b83e0b5959f14b750
a17600765 <Vetchinnikov.P.E@sberbank.ru>
2020-04-26 12:39:08 +0300

    Готовы тесты DAO жанров и авторов

---
+++ bookshelf/src/test/java/ru/otus/homework/dao/impl/GenreDaoImplTest.java
@@ -0,0 +1,51 @@
+package ru.otus.homework.dao.impl;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
+import ru.otus.homework.model.Genre;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JdbcTest
+@Import(GenreDaoImpl.class)
+@DisplayName(value = "DAO жанров должен:")
+class GenreDaoImplTest {
+
+    @Autowired
+    private GenreDaoImpl dao;
+
+    @Test
+    @DisplayName(value = "возвращать из базы сохраненный жанр;")
+    void shouldReturnInsertedGenre() {
+        long insertedGenre = dao.insert(Genre.builder().name("Test Genre").build());
+        assertThat(dao.findById(insertedGenre).getName()).isEqualTo("Test Genre");
+
+    }
+
+    @Test
+    @DisplayName(value = "возвращать из базы жанр по его ID;")
+    void shouldReturnGenreById() {
+        assertThat(dao.findById(1).getName()).isEqualTo("Fantasy");
+    }
+
+    @Test
+    @DisplayName(value = "возвращать из базы жанр по его имени;")
+    void shouldReturnGenreByName() {
+        assertThat(dao.findByName("Fantasy").getId()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName(value = "возвращать из базы полный список жанров;")
+    void shouldReturnFullListOfStoredGenres() {
+        assertThat(dao.findAll().size()).isGreaterThanOrEqualTo(7);
+    }
+
+    @Test
+    @DisplayName(value = "возвращать из базы список жанров, привязанных к определенной книге;")
+    void shouldReturnListOfGenresBoundToBook() {
+        assertThat(dao.findAllByBookId(4).size()).isEqualTo(4);
+    }
+}
