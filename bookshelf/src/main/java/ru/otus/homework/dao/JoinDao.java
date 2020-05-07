75aef268273cb49c42f4d301155f2d53256ebe9e
a17600765 <Vetchinnikov.P.E@sberbank.ru>
2020-05-07 21:35:00 +0300

    Удалены DAO и их тесты, а также RowMappers

--- bookshelf/src/main/java/ru/otus/homework/dao/JoinDao.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.homework.dao;
-
-public interface JoinDao {
-
-    long bookAuthorBindingCreate(long bookId, long authorId);
-    long bookGenreBindingCreate(long bookId, long genreId);
-    int bookBindingsDelete(long bookId);
-
-}
