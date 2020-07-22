b79aead96e26bb4d8f09118cb45a81c585126017
requiemail <48630429+requiemail@users.noreply.github.com>
2020-07-22 15:49:23 +0300

    Реализована form-based аутентификация (#10)
    Co-authored-by: a17600765 <Vetchinnikov.P.E@sberbank.ru>

---
+++ bookshelf/src/main/java/ru/otus/homework/security/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package ru.otus.homework.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure( HttpSecurity http ) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+        ;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
---
+++ bookshelf/src/main/java/ru/otus/homework/security/service/DaoUserDetailsService.java
@@ -0,0 +1,73 @@
+package ru.otus.homework.security.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.otus.homework.security.model.User;
+import ru.otus.homework.security.repository.UserRepository;
+
+import java.util.Collection;
+
+@RequiredArgsConstructor
+@Service
+public class DaoUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new DaoUserPrincipal(user);
+    }
+
+    public static class DaoUserPrincipal implements UserDetails {
+
+        private User user;
+
+        public DaoUserPrincipal(User user) {
+            this.user = user;
+        }
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            return null;
+        }
+
+        @Override
+        public String getPassword() {
+            return user.getPassword();
+        }
+
+        @Override
+        public String getUsername() {
+            return user.getUsername();
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
+    }
+
+}
