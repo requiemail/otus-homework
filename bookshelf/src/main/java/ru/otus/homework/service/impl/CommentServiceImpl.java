88d254054a127b5ee43d7dff306c46b938d7f49b
a17600765 <Vetchinnikov.P.E@sberbank.ru>
2020-08-12 01:00:54 +0300

    Добавлен метод паузы потока, добавлены аннотации Hystrix Javanica, добавлен fallback-метод

--- bookshelf/src/main/java/ru/otus/homework/service/impl/CommentServiceImpl.java
+++ bookshelf/src/main/java/ru/otus/homework/service/impl/CommentServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.otus.homework.service.impl;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.otus.homework.error.NotFoundException;
@@ -7,7 +9,10 @@ import ru.otus.homework.model.Comment;
 import ru.otus.homework.repository.CommentRepository;
 import ru.otus.homework.service.CommentService;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 @RequiredArgsConstructor
 @Service
@@ -16,13 +21,42 @@ public class CommentServiceImpl implements CommentService {
     private final CommentRepository repository;
 
     @Override
+    @HystrixCommand(commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+    })
     public Comment getById(Long id) {
+        sleepRandomly();
         return repository.findById(id).orElseThrow(() -> new NotFoundException(String.format("Comment with id %d not found", id)));
     }
 
     @Override
+    @HystrixCommand(
+            fallbackMethod = "getFallbackComment",
+            commandProperties = {
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+            })
     public List<Comment> getAll() {
+        sleepRandomly();
         return repository.findAll();
+    }
+
+    public List<Comment> getFallbackComment() {
+        return Collections.singletonList(Comment.builder()
+                .commentAuthor("system")
+                .commentText("***all comments temporary unavailable, try later***")
+                .build());
+    }
+
+    private void sleepRandomly() {
+        Random rand = new Random();
+        int randomInt = rand.nextInt(3) + 1;
+        if (randomInt == 3) {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
