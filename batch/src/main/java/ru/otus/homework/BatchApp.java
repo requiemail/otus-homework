21b9ff044f871066f5273b4c7bae52994f12b5ef
a17600765 <Vetchinnikov.P.E@sberbank.ru>
2020-08-26 23:21:21 +0300

    Переименован главный класс

--- batch/src/main/java/ru/otus/homework/BatchApp.java
+++ batch/src/main/java/ru/otus/homework/BatchApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MongoBookShelf {
+public class BatchApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(MongoBookShelf.class, args);
+        SpringApplication.run(BatchApp.class, args);
     }
 
 }
